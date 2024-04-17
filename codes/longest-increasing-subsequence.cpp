1e7db9e197a7712d0476af372e03ead67d9b102f
OFShare <OFShare@outlook.com>
2024-04-17 22:33:47 +0800

    update

--- codes/longest-increasing-subsequence.cpp
+++ codes/longest-increasing-subsequence.cpp
@@ -3,6 +3,7 @@ public:
     static const int N = 3e3 + 5;
     // dp[i]表示前i个元素[0: i], 以第i个元素结尾时，最长单调上升子序列长度
     int dp[N];
+    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size(), ans = 1;
         for (int i = 0; i < n; ++i) {
